960f9a6e46be278af3c42684a2c9cd19e2f22bcb
Ivy Mageto <Nyasu@MirLeos-iPhone.localdomain>
2019-10-21 15:55:42 -0700

    made changes

--- sprint-three/Script/comments.js
+++ sprint-three/Script/comments.js
@@ -1,23 +1,23 @@
-// const comments = [
-//   {
-//     name: "Micheal Lyons",
-//     date: "12/18/2018",
-//     comment:
-//       "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
-//   },
-//   {
-//     name: "Gary Wong",
-//     date: "12/12/2018",
-//     comment:
-//       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
-//   },
-//   {
-//     name: "Theodore Duncan",
-//     date: "11/15/2018",
-//     comment:
-//       "How can someone be so good!!! You can tell he lives for this and loves to do it every day. rEverytime I see him I feel instantly happy! He’s definitely my favorite ever!"
-//   }
-// ];
+const comments = [
+  {
+    name: "Micheal Lyons",
+    date: "12/18/2018",
+    comment:
+      "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+  },
+  {
+    name: "Gary Wong",
+    date: "12/12/2018",
+    comment:
+      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+  },
+  {
+    name: "Theodore Duncan",
+    date: "11/15/2018",
+    comment:
+      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. rEverytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+  }
+];
 
 let form = document.querySelector(".comment__form");
 form.addEventListener("submit", function(e) {
